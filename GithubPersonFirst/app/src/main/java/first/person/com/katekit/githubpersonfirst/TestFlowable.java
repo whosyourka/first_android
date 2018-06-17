42ae2ba1b30fc442acfd1ab9e7ffe1b260972f4c
huangmcb <huangmcb@landicorp.com>
2018-06-17 10:52:40 +0800

    feat:新增rxjava部分操作符

--- GithubPersonFirst/app/src/main/java/first/person/com/katekit/githubpersonfirst/TestFlowable.java
+++ GithubPersonFirst/app/src/main/java/first/person/com/katekit/githubpersonfirst/TestFlowable.java
@@ -1,25 +1,104 @@
 package first.person.com.katekit.githubpersonfirst;
 
+import java.util.concurrent.TimeUnit;
+
 import io.reactivex.Flowable;
+import io.reactivex.Scheduler;
+import io.reactivex.flowables.ConnectableFlowable;
 
 /**
  * Created by 黄明灿 on 2018/6/12 19:41.
  * Describe :
  */
 public class TestFlowable {
-    public static Flowable myAll(){
-        return Flowable.just("","ss")
-                .map(it->{
-                    System.out.print("it:"+it);
+    public static Flowable myAll() {
+        return Flowable.just("", "ss")
+                .map(it -> {
+                    System.out.println("it:" + it);
                     return it;
                 }).all(it -> !(it.isEmpty())).toFlowable();
     }
 
-    public static Flowable myContains(){
-        return Flowable.just("11","ss","22","ss")
-                .map(it->{
-                    System.out.print("it:"+it);
+    public static Flowable myContains() {
+        return Flowable.just("11", "ss", "22", "ss")
+                .map(it -> {
+                    System.out.println("it:" + it);
                     return it;
-                }).contains("ss").toFlowable();
+                }).contains("ss").map(it -> {
+                    System.out.println("it contains:" + it);
+                    return it;
+                }).toFlowable();
+    }
+
+    public static Flowable myEmpty() {
+        return Flowable.just("11", "ss", "22", "ss")
+                .map(it -> {
+                    System.out.println("it:" + it);
+                    return it;
+                }).filter(it -> false).isEmpty().map(it -> {
+                    System.out.println("it contains:" + it);
+                    return it;
+                }).toFlowable();
+    }
+
+    public static Flowable myDefaultEmpty() {
+        return Flowable.just("11", "ss", "22", "ss")
+                .map(it -> {
+                    System.out.println("it:" + it);
+                    return it;
+                }).filter(it -> false).defaultIfEmpty("haha").map(it -> {
+                    System.out.println("it contains:" + it);
+                    return it;
+                });
+    }
+
+    public static Flowable myPublish() {
+        return Flowable.just("11")
+                .map(it -> {
+                    System.out.println("it:" + it);
+                    return it;
+                });
+    }
+
+    public static Flowable myRangeSample(Scheduler scheduler) {
+        return Flowable.range(0, 1000).sample(10, TimeUnit.SECONDS,scheduler)
+                .map(it -> {
+                    System.out.println("it:" + it);
+                    return it;
+                });
+    }
+
+    public static Flowable<Long> myInterval(Scheduler scheduler) {
+        return Flowable.merge(Flowable.just(-1L),Flowable.interval(10, TimeUnit.SECONDS,scheduler))
+                .map(it -> {
+                    System.out.println("it:" + it);
+                    return it;
+                }).take(100);
+    }
+
+    public static ConnectableFlowable<Long> myConnect(Scheduler scheduler) {
+        return Flowable.merge(Flowable.just(-1L),Flowable.interval(10, TimeUnit.SECONDS,scheduler))
+                .map(it -> {
+                    System.out.println("it:" + it);
+                    return it;
+                }).take(100).publish();
+    }
+    public static Flowable<Long> myRefCount(Scheduler scheduler) {
+        return Flowable.merge(Flowable.just(-1L),Flowable.interval(10, TimeUnit.SECONDS,scheduler))
+                .map(it -> {
+                    System.out.println("it:" + it);
+                    return it;
+                }).take(100)
+                .publish().refCount();
+//                .share();
+    }
+    public static ConnectableFlowable<Long> myReplay(Scheduler scheduler) {
+        return Flowable.merge(Flowable.just(-1L),Flowable.interval(10, TimeUnit.SECONDS,scheduler))
+                .map(it -> {
+                    System.out.println("it:" + it);
+                    return it;
+                }).take(100)
+                .replay();
+//                .share();
     }
 }
--- GithubPersonFirst/app/src/test/java/first/person/com/katekit/githubpersonfirst/TestFlowableTest.java
+++ GithubPersonFirst/app/src/test/java/first/person/com/katekit/githubpersonfirst/TestFlowableTest.java
@@ -2,19 +2,143 @@ package first.person.com.katekit.githubpersonfirst;
 
 import org.junit.Test;
 
+import io.reactivex.Flowable;
+import io.reactivex.flowables.ConnectableFlowable;
+import io.reactivex.schedulers.TestScheduler;
+import io.reactivex.subscribers.TestSubscriber;
+
+import static java.util.concurrent.TimeUnit.SECONDS;
+
 /**
  * Created by 黄明灿 on 2018/6/12 19:43.
  * Describe :
  */
 public class TestFlowableTest {
     @Test
-    public void testMyAll() {
-        TestFlowable.myAll().test().assertValue(true).assertValue(false);
-        TestFlowable.myAll().test().assertValue(false).assertValue(true);
+    public void myAll() {
+        TestFlowable.myAll().test().assertValue(true);
+        TestFlowable.myAll().test().assertValue(false);
     }
 
     @Test
     public void myContains() {
-        TestFlowable.myAll().test().assertValue(true).assertValue(false);
+        TestFlowable.myContains().test().assertValue(true);
     }
+
+    @Test
+    public void myEmpty() {
+        TestFlowable.myEmpty().test().assertValue(true);
+    }
+
+    @Test
+    public void myDefaultEmpty() {
+        TestFlowable.myDefaultEmpty().test().assertValue("haha");
+    }
+
+    @Test
+    public void myPublish() {
+        TestFlowable.myPublish().test().assertComplete();
+        TestFlowable.myPublish().test().assertComplete();
+    }
+
+
+
+    @Test
+    public void myRangeSample() {
+        TestScheduler scheduler = new TestScheduler();
+        TestSubscriber<Long> testObserver = TestFlowable.myRangeSample(scheduler).test();
+
+        scheduler.advanceTimeTo(12, SECONDS);
+        testObserver.assertValueCount(1000);
+
+//        scheduler.advanceTimeTo(9, SECONDS);
+//        testObserver.assertValueCount(0);
+    }
+
+    @Test
+    public void myInterval() {
+        TestScheduler scheduler = new TestScheduler();
+        TestSubscriber<Long> testObserver = TestFlowable.myInterval(scheduler).test();
+
+
+        scheduler.advanceTimeBy(9, SECONDS);
+        testObserver.assertValueCount(1);
+        scheduler.advanceTimeBy(1, SECONDS);
+        testObserver.assertValueCount(2);
+
+        scheduler.advanceTimeTo(10, SECONDS);
+        testObserver.assertValueCount(2);
+        scheduler.advanceTimeTo(10, SECONDS);
+        testObserver.assertValueCount(2);
+
+//        scheduler.advanceTimeTo(1000, SECONDS);
+//        testObserver.assertValueCount(100)
+//            .assertValueAt(0,it -> it == 0)
+//            .assertValueAt(3,it -> it == 3);
+    }
+    @Test
+    public void myConnect() {
+        TestScheduler scheduler = new TestScheduler();
+        ConnectableFlowable<Long> flowable = TestFlowable.myConnect(scheduler);
+        TestSubscriber testObserver = flowable.test();
+        flowable.subscribe(it->{
+            System.out.println("haha");
+        });
+        flowable.subscribe(it->{
+            System.out.println("lala");
+        });
+
+        scheduler.advanceTimeBy(10, SECONDS);
+        testObserver.assertValueCount(0);
+        flowable.connect();
+        scheduler.advanceTimeBy(10, SECONDS);
+        testObserver.assertValueCount(2);
+    }
+    @Test
+    public void myRefCount() {
+        TestScheduler scheduler = new TestScheduler();
+        Flowable<Long> flowable = TestFlowable.myRefCount(scheduler);
+        TestSubscriber testObserver = flowable.test();
+        flowable.subscribe(it->{
+            System.out.println("haha");
+        });
+        flowable.subscribe(it->{
+            System.out.println("lala");
+        });
+
+        scheduler.advanceTimeBy(10, SECONDS);
+        testObserver.assertValueCount(2);
+        flowable.subscribe(it->{
+            System.out.println("gaga");
+        });
+        scheduler.advanceTimeBy(10, SECONDS);
+        testObserver.assertValueCount(3);
+
+    }
+    @Test
+    public void myReplay() {
+        TestScheduler scheduler = new TestScheduler();
+        ConnectableFlowable<Long> flowable = TestFlowable.myReplay(scheduler);
+        TestSubscriber testObserver = flowable.test();
+
+        flowable.subscribe(it->{
+            System.out.println("haha");
+        });
+        flowable.subscribe(it->{
+            System.out.println("lala");
+        });
+        flowable.connect();
+        scheduler.advanceTimeBy(20, SECONDS);
+        testObserver.assertValueCount(3);
+
+        flowable.subscribe(it->{//会多调用2次
+            System.out.println("gaga");
+        });
+        scheduler.advanceTimeBy(10, SECONDS);
+        testObserver.assertValueCount(4);
+
+    }
+
+
+
 }
